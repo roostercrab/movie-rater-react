ae53fb5f22387e808fb1c2fdb7d8d53f6def7cf7
roostercrab <jesseshea498@gmail.com>
2021-04-26 16:09:25 -0700

    git changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [movies, setMovie] = useState([])
+
+  useEffect(() => {
+    fetch('127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Token ',
+      },
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Movie Rater</h1>
+      <div className='layout'>
+        <div>Movie List</div>
+        <div>Description</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
